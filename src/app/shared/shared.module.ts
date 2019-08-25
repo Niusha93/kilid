d44494b769f4254ddcb409a941497f4fe8832001
Siavash Ghanbari <sia.qnbr@gmail.com>
2019-08-25 09:27:45 +0430

    add flex package toi shared-module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,24 +1,11 @@
-import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MaterialModule } from './material.module';
-import { FlexLayoutModule } from '@angular/flex-layout';
-
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MaterialModule } from "./material.module";
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 @NgModule({
-  declarations: [
-
-  ],
-  imports: [
-    FormsModule,
-    ReactiveFormsModule,
-    MaterialModule,
-    FlexLayoutModule,
-  ],
-  exports: [
-    FormsModule,
-    ReactiveFormsModule,
-    MaterialModule,
-    FlexLayoutModule
-  ]
+  declarations: [],
+  imports: [FormsModule, ReactiveFormsModule, MaterialModule, FlexLayoutModule],
+  exports: [FormsModule, ReactiveFormsModule, MaterialModule, FlexLayoutModule]
 })
-export class SharedModule { }
+export class SharedModule {}
